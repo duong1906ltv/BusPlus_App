820f8d030a022980812f1744aad3145ee204eb52
hothihieu <hothihieu2404@gmail.com>
2023-07-10 09:36:13 +0700

    Show Admin notices

--- actions/noti.js
+++ actions/noti.js
@@ -1,5 +1,6 @@
 import { Toast } from 'react-native-toast-message/lib/src/Toast.js'
 import api from '../services/api1.js'
+import { GET_CURRENT_ADMIN_NOTICE } from './types.js'
 
 export const getListNoti = () => {
   return (dispatch, getState) => {
@@ -19,6 +20,25 @@ export const getListNoti = () => {
           type: 'GET_NOTI_FAILURE',
           payload: error.response.data,
         })
+      })
+  }
+}
+
+export const getCurrentAdminNotice = () => {
+  return (dispatch, getState) => {
+    const state = getState()
+
+    api
+      .get(`/noti/admin/current`)
+      .then((response) => {
+        console.log("response.data", response.data);
+        dispatch({
+          type: GET_CURRENT_ADMIN_NOTICE,
+          payload: response.data,
+        })
+      })
+      .catch((error) => {
+        console.log("error", error);
       })
   }
 }
